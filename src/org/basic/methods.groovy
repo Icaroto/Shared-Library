7f4e2c51b9c1bac5cbf3a82fdbbd2893c4c7af76
Icaro Torres <icaro.torres@fourth.com>
2018-05-21 15:26:16 +0100

    back to other git step

--- src/org/basic/methods.groovy
+++ src/org/basic/methods.groovy
@@ -1,15 +1,19 @@
 #!groovy
 package org.basic;
 
-def checkOutFrom(branch) {
-  checkout([
-		$class: 'GitSCM',
-    url: "https://github.com/Icaroto/Jenkins.git",
-		branches: [[name: "*/${branch}"]], 
-		extensions: scm.extensions + [[$class: 'CleanCheckout']],
-		userRemoteConfigs: scm.userRemoteConfigs
-		])
-		echo "Retrieved $env.BRANCH_NAME in workspace: $env.WORKSPACE"
+def checkOutFrom(repo) {
+  git url: "git@github.com:Icaroto/${repo}.git"
 }
+
+// def checkOutFrom(branch) {
+//   checkout([
+// 		$class: 'GitSCM',
+//     url: "https://github.com/Icaroto/Jenkins.git",
+// 		branches: [[name: "*/${branch}"]], 
+// 		extensions: scm.extensions + [[$class: 'CleanCheckout']],
+// 		userRemoteConfigs: scm.userRemoteConfigs
+// 		])
+// 		echo "Retrieved $env.BRANCH_NAME in workspace: $env.WORKSPACE"
+// }
 
 return this
