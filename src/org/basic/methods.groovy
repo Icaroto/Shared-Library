ebe928d8e725919181c1463f2ae5a3b1a380c675
Icaro Torres <icaro.torres@fourth.com>
2018-05-21 15:12:21 +0100

    added different git method

--- src/org/basic/methods.groovy
+++ src/org/basic/methods.groovy
@@ -1,8 +1,15 @@
-#! groovy
+#!groovy
 package org.basic;
 
-def checkOutFrom(repo) {
-  git url: "git@github.com:Icaroto/${repo}"
+def checkOutFrom(branch) {
+  checkout([
+		$class: 'GitSCM',
+    url: "https://github.com/Icaroto/Jenkins.git",
+		branches: [[name: "*/${branch}"]], 
+		extensions: scm.extensions + [[$class: 'CleanCheckout']],
+		userRemoteConfigs: scm.userRemoteConfigs
+		])
+		echo "Retrieved $env.BRANCH_NAME in workspace: $env.WORKSPACE"
 }
 
 return this
