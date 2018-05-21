27052b004d144f851a483dafbefcaecd7c82542a
Icaro Torres <icaro.torres@fourth.com>
2018-05-21 14:11:17 +0100

    Initial test

---
+++ vars/utils.groovy
@@ -0,0 +1,13 @@
+#! groovy
+
+def info(message) {
+    echo "INFO: ${message}"
+}
+
+def warning(message) {
+    echo "WARNING: ${message}"
+}
+
+def error(message) {
+    echo "ERROR: ${message}"
+}
