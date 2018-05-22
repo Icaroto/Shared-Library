765dfcd13fb8a48de4a3bde057219ff20491d663
Icaroto <icarotor@gmail.com>
2018-05-22 11:50:46 +0100

    Update global.groovy

--- vars/global.groovy
+++ vars/global.groovy
@@ -1,13 +1,3 @@
 #! groovy
 def NugetPath = ''
 def OtherPath = ''
-
-def call(Map config) {
-    if (config.NugetPath != ''){
-        NugetPath = config.NugetPath;
-    }
-
-    if (config.OtherPath != ''){
-        OtherPath = config.OtherPath;
-    }   
-}
